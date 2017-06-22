83062ed01ffb3fe00ae920ca3c4ba4527fafaf3e
ThuyNT13 <ngo.thuy38@gmail.com>
2017-06-21 19:22:03 -0500

    Add fizzbuzz methods and tests; pass all tests

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,11 @@
 =begin
 Write a program that prints integers from 1 to 100 using FizzBuzz rules.
 
-Given an integer input, print:
-- "Fizz" if it's a multiple of 3
-- "Buzz" if it's a mutiple of 5
-- "FizzBuzz" if it's a mutliple of 3 and 5 (15)
-- the number if neither a mutiple of 3 nor 5
+  Given an integer input, print:
+  - "Fizz" if it's a multiple of 3
+  - "Buzz" if it's a mutiple of 5
+  - "FizzBuzz" if it's a mutliple of 3 and 5 (15)
+  - the number if neither a mutiple of 3 nor 5
 
 BONUS:
 - Refactor for DRYness.
@@ -14,3 +14,24 @@ EX: "Foo" for multiples of 2, "Bar" for mutliples of 7, "FooBar" for multiples o
 
 Start by pseudocoding the tests. In concise terms, describe what is expected. Within what context? if it's not concise, break it down further.
 =end
+
+def is_divisible_by_3?(num)
+  num % 3 == 0
+end
+
+def is_divisible_by_5?(num)
+  num % 5 == 0
+end
+
+def fizzbuzz(num)
+  return "FizzBuzz" if (is_divisible_by_3?(num) && is_divisible_by_5?(num))
+  return "Fizz" if is_divisible_by_3?(num)
+  return "Buzz" if is_divisible_by_5?(num)
+  num
+end
+
+def print_fizzbuzz(maxNum)
+  1.upto(maxNum) { |i| puts fizzbuzz(i).to_s }
+end
+
+print_fizzbuzz(25)
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,35 @@
 require_relative 'spec_helper.rb'
 
 describe 'FizzBuzz' do
+  describe 'is_divisible_by_3?()' do
+    it 'returns true if divisible by 3' do
+      expect(is_divisible_by_3?(21)).to be(true)
+    end
+    it 'returns false if not divisible by 3' do
+      expect(is_divisible_by_3?(4)).to be(false)
+    end
+  end
+  describe 'is_divisible_by_5?()' do
+    it 'returns true if divisible by 5' do
+      expect(is_divisible_by_5?(45)).to be(true)
+    end
+    it 'returns false if not divisible by 5' do
+      expect(is_divisible_by_5?(38)).to be(false)
+    end
+  end
 
+  describe 'fizzbuzz() prints' do
+    it 'the input integer for numbers divisible by neither 3 nor 5.' do
+      expect(fizzbuzz(37)).to eq(37)
+    end
+    it '"Fizz" for numbers divisible by 3.' do
+      expect(fizzbuzz(6)).to eq("Fizz")
+    end
+    it '"Buzz" for numbers divisible by 5.' do
+      expect(fizzbuzz(100)).to eq("Buzz")
+    end
+    it '"FizzBuzz" for numbers divisible by 3 and 5.' do
+      expect(fizzbuzz(45)).to eq("FizzBuzz")
+    end
+  end
 end
